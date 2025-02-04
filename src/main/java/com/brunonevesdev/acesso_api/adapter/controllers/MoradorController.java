4e9d078e58ad0dff8c4e7f7a72d9ceffd5bd19b2
Bruno_Neves <nevesbruno814@gmail.com>
2025-02-03 20:59:36 -0400

    adicionado o parametro que vai receber os dados via body

--- src/main/java/com/brunonevesdev/acesso_api/adapter/controllers/MoradorController.java
+++ src/main/java/com/brunonevesdev/acesso_api/adapter/controllers/MoradorController.java
@@ -6,10 +6,7 @@ import com.brunonevesdev.acesso_api.core.ports.entrada.MoradorServicePort;
 import com.brunonevesdev.acesso_api.domain.Morador;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/moradores")
@@ -21,7 +18,7 @@ public class MoradorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MoradorDTO create(MoradorDTO moradorDTO){
+    public MoradorDTO create(@RequestBody MoradorDTO moradorDTO){
 
         Morador moradorNovo = moradorServicePort.createMorador(moradorConverter.toDomain(moradorDTO));
         return  moradorConverter.toDTO(moradorNovo);
