5224ab368f125dda2cb8ec39eb99b231bbbf19ac
Bruno_Neves <nevesbruno814@gmail.com>
2025-02-03 20:37:37 -0400

    criação da controller e DTO do morador

---
+++ src/main/java/com/brunonevesdev/acesso_api/adapter/controllers/MoradorController.java
@@ -0,0 +1,11 @@
+package com.brunonevesdev.acesso_api.adapter.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/moradores")
+@RequiredArgsConstructor
+public class MoradorController {
+}
---
+++ src/main/java/com/brunonevesdev/acesso_api/adapter/dtos/MoradorDTO.java
@@ -0,0 +1,18 @@
+package com.brunonevesdev.acesso_api.adapter.dtos;
+
+import com.brunonevesdev.acesso_api.domain.Pessoa;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MoradorDTO {
+    private Long id;
+    private String cpf;
+    private String endereco;
+    private String celular;
+    private Pessoa pessoa;
+
+}
